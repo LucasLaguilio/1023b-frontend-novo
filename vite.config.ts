a009f97e13d5a43d993af5855b999c19cfc35a63
Lucas Laguilio <laguiliolucas@gmail.com>
2025-09-15 08:27:19 -0400

    Configurando Variáveis de ambiente para o backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import 'dotenv/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+  proxy: {
+  '/api': {
+        target: process.env.API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+
+  },  
 })
